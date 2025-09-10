690d61e411ac2307455b2c15777ba4d0d733ec91
SanketLohar <loharsanket105@gmail.com>
2025-09-10 12:45:06 +0530

    refactor: consolidate component scanning

--- demo/src/main/java/com/backend/demo/DemoApplication.java
+++ demo/src/main/java/com/backend/demo/DemoApplication.java
@@ -1,28 +1,19 @@
 package com.backend.demo;
 
-import io.github.cdimascio.dotenv.Dotenv;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.domain.EntityScan;
-import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
-@SpringBootApplication
-@EntityScan("com.backend.demo.model")
-@EnableJpaRepositories("com.backend.demo.repository") // <-- Add this line
-
+/**
+ * This single annotation tells Spring Boot to scan the entire
+ * "com.backend.demo" package and all its sub-packages for every type of
+ * component (Controllers, Services, Entities, Repositories), fixing all
+ * previous startup errors.
+ */
+@SpringBootApplication(scanBasePackages = "com.backend.demo")
 public class DemoApplication {
+
 	public static void main(String[] args) {
-
-		Dotenv dotenv = Dotenv.configure()
-				.ignoreIfMissing() // Doesn't crash if .env is not there
-				.load();
-
-
-		dotenv.entries().forEach(entry -> {
-			System.setProperty(entry.getKey(), entry.getValue());
-		});
-
-
 		SpringApplication.run(DemoApplication.class, args);
 	}
+
 }
