b32390f7d8f5ceed6f8b2c4d8666034e6516c710
SanketLohar <loharsanket105@gmail.com>
2025-09-10 12:33:08 +0530

    fix: add @EnableJpaRepositories to locate repositories

--- demo/src/main/java/com/backend/demo/DemoApplication.java
+++ demo/src/main/java/com/backend/demo/DemoApplication.java
@@ -4,9 +4,12 @@ import io.github.cdimascio.dotenv.Dotenv;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
 @EntityScan("com.backend.demo.model")
+@EnableJpaRepositories("com.backend.demo.repository") // <-- Add this line
+
 public class DemoApplication {
 	public static void main(String[] args) {
 
