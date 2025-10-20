007b49ec2e203896bcb3994d648e19d49a83d485
SanketLohar <loharsanket105@gmail.com>
2025-10-20 22:14:32 +0530

    updated

--- demo/src/main/java/com/backend/demo/config/SecurityConfig.java
+++ demo/src/main/java/com/backend/demo/config/SecurityConfig.java
@@ -43,7 +43,7 @@ public class SecurityConfig {
         CorsConfiguration configuration = new CorsConfiguration();
         configuration.setAllowedOriginPatterns(List.of(
                 "http://localhost:5173",
-                "https://*-sanketlohars-projects.vercel.app"
+                "https://tonechanger-eta.vercel.app"
         ));
         configuration.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "OPTIONS"));
         configuration.setAllowedHeaders(List.of("*"));
